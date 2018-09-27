3438bbd235614a35b8aa72b0b21b83f3aa99b71e
Ubuntu <ubuntu@ip-172-31-89-136.ec2.internal>
2018-09-27 22:13:55 +0000

    fix path

--- startwebsocket.sh
+++ startwebsocket.sh
@@ -1,3 +1,3 @@
-cd /home/ubuntu/simple-websocket-server/SimpleWebSocketServer
+cd simple-websocket-server/SimpleWebSocketServer
 nohup python SimpleExampleServer.py --example chat &
 nohup python -m SimpleHTTPServer 7000 &
